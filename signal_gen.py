67efce4c43e2e2b3bdd4abb38ad005be1daf4c47
Nathan Willey <willeyna@msu.edu>
2021-05-21 13:50:01 -0400

    typo fix

--- signal_gen.py
+++ signal_gen.py
@@ -5,7 +5,7 @@ tester = pickle.load(open('./Testers/' + sys.argv[1],'rb'))
 signal_TS = np.zeros([tester.nper, len(tester.Methods)])
 
 t1= datetime.datetime.now()
-for i in (np.arange(self.signal_trials)):
+for i in (np.arange(tester.signal_trials)):
     signal_TS[i] = tester.test_methods(tester.ra, tester.dec, ninj_t = tester.ninj_t, ninj_c = tester.ninj_c, inj_ra = tester.inj_ra, inj_dec = tester.inj_dec)
 t2= datetime.datetime.now()
 dt=(t2-t1).total_seconds()
